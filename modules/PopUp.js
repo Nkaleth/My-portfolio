a468292e8e077a46b6777f7e65a6fbf895d71a97
Nkaleth <nseguralu@gmail.com>
2023-03-22 11:04:20 -0500

    FIX: Styling for desktop version of projects

--- modules/PopUp.js
+++ modules/PopUp.js
@@ -1,5 +1,5 @@
 const ClosePopUp = () => {
-  const PopUp = document.querySelector('.MobilePopUp');
+  const PopUp = document.querySelector('.PopUp');
   PopUp.style.display = 'none';
 };
 
@@ -10,10 +10,12 @@ const createStrProjectMob = (projects, id) => {
   projects.forEach((proj) => {
     if (proj.id === i * 1) {
       if (window.innerWidth < 768) {
-        string = `<div class="whiteBackground">
+        string = `<div class="whiteBackgroundDesktop">
                         <article class="ProjectPopUp">
                           <button class="btMobWhite"><img src="img/X_white.svg" alt="ButtonClose"></button>
-                          <img class="imagePopMob" src="${proj.featuredImage[0]}" alt="ProjectImage">
+                          <div class="ProjImageBox">
+                          <img class="imagePopMob" src="${proj.featuredImage}" alt="ProjectImage">
+                          </div>
                           <h3 class="Proj-Text">${proj.name}</h3>
                           <ul>
                             <li class="tag">${proj.technologies[0]}</li>
@@ -27,34 +29,39 @@ const createStrProjectMob = (projects, id) => {
                       </div>`;
       } else {
         string = `<div class="whiteBackground">
-                        <button class="btMobDesktop">
-                        <img src="img/proy_button_desktop.svg" alt="ButtonClose"></button>
                         <article class="ProjectPopUpDesk">
-                          <img class="proj-image" src="${proj.featuredImage[1]}" alt="ProjectImage">
-                          <div class="h3_ButtonsPopUpDesk">
-                            <h3 class="ProjTextPopUpDeskt">${proj.name}</h3>
-                          <div class="buttonMobPopupDesk">
-                        <a href="${proj.liveVersion}"><img class="buttonPopUpDesktop" src="img/See_live.svg" alt="ButtonEnable"></a>
-                        <a href="${proj.linkSource}"><img class="buttonPopUpDesktop" src="img/See_source.svg" alt="ButtonEnable"></a>
+                          <div class="ProjImageBoxPopUp">
+                            <button class="btMobDesktop">
+                            <img src="img/proy_button_desktop.svg" alt="ButtonClose">
+                            </button>
+                            <img class="proj_imagePopUp" src="${proj.featuredImage}" alt="ProjectImage">
                           </div>
+                          <div class="detailsContainer">
+                              <div class="h3_ButtonsPopUpDesk">
+                              <h3 class="ProjTextPopUpDeskt">${proj.name}</h3>
+                            <div class="buttonMobPopupDesk">
+                            <a href="${proj.liveVersion}"><img class="buttonPopUpDesktop" src="img/See_live.svg" alt="ButtonEnable"></a>
+                            <a href="${proj.linkSource}"><img class="buttonPopUpDesktop" src="img/See_source.svg" alt="ButtonEnable"></a>
+                            </div>
+                            </div>
+                            <ul>
+                              <li class="tagDesktop">${proj.technologies[0]}</li>
+                              <li class="tagDesktop">${proj.technologies[1]}</li>
+                              <li class="tagDesktop">${proj.technologies[2]}</li>
+                              <li class="tagDesktop">${proj.technologies[3]}</li>
+                              <li class="tagDesktop">${proj.technologies[4]}</li>
+                              <li class="tagDesktop">${proj.technologies[5]}</li>
+                            </ul>
+                            <p class = "popUpDescription" >${proj.description}</p>                         
                           </div>
-                          <ul>
-                            <li class="tagDesktop">${proj.technologies[0]}</li>
-                            <li class="tagDesktop">${proj.technologies[1]}</li>
-                            <li class="tagDesktop">${proj.technologies[2]}</li>
-                            <li class="tagDesktop">${proj.technologies[3]}</li>
-                            <li class="tagDesktop">${proj.technologies[4]}</li>
-                            <li class="tagDesktop">${proj.technologies[5]}</li>
-                          </ul>
-                          <p class = "popUpDescription" >${proj.description}</p>
                         </article>
-                      </div>`;
+                  </div>`;
       }
     }
   });
   const body = document.querySelector('.body');
   const PopUp = document.createElement('div');
-  PopUp.classList.add('MobilePopUp');
+  PopUp.classList.add('PopUp');
   PopUp.innerHTML = string;
   body.prepend(PopUp);
   if (window.innerWidth < 768) {
--- modules/projects.js
+++ modules/projects.js
@@ -6,7 +6,7 @@ const projects = [
     id: 1,
     name: 'Conference page',
     description: 'In this project I build a page based on an online website for a conference.',
-    featuredImage: ['img/capstone1_mobile.png', 'img/capston1_desktop.png'],
+    featuredImage: 'img/capstone1.png',
     technologies: ['Ruby on rails', 'css', 'JavScript', 'Codekit', 'Github', 'Bootstrap', 'Terminal', 'Codepen'],
     liveVersion: 'https://nkaleth.github.io/capstone_project1/',
     linkSource: 'https://github.com/Nkaleth/capstone_project1',
@@ -15,7 +15,7 @@ const projects = [
     id: 2,
     name: 'Keeping track of hundreds of components',
     description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
-    featuredImage: ['img/Snapshoot_Portfolio_mobile.jpg', 'img/Snapshoot_Portfolio_desktop.png'],
+    featuredImage: 'img/leaderboard.png',
     technologies: ['Ruby on rails', 'css', 'JavScript', 'Codekit', 'Github', 'Bootstrap', 'Terminal', 'Codepen'],
     liveVersion: '#url',
     linkSource: '#url',
@@ -24,7 +24,7 @@ const projects = [
     id: 3,
     name: 'Keeping track of hundreds of components',
     description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
-    featuredImage: ['img/Snapshoot_Portfolio_mobile.jpg', 'img/Snapshoot_Portfolio_desktop.png'],
+    featuredImage: 'img/art_gallery.png',
     technologies: ['Ruby on rails', 'css', 'JavScript', 'Codekit', 'Github', 'Bootstrap', 'Terminal', 'Codepen'],
     liveVersion: '#url',
     linkSource: '#url',
@@ -64,21 +64,25 @@ const loadProjects = () => {
 
   projects.forEach((proj) => {
     string += `<article class="project">
-                    <img class="proj-image" src="${proj.featuredImage[0]}" alt="ProjectImage">
+                    <div class="ProjImageBox">
+                    <img class="proj-image" src="${proj.featuredImage}" alt="ProjectImage">
+                    </div>
                     <h3 class="Proj-Text">${proj.name}</h3>
                     <ul>
                       <li class="tag">${proj.technologies[0]}</li>
                       <li class="tag">${proj.technologies[1]}</li>
                       <li class="tag">${proj.technologies[2]}</li>
                     </ul>
-                    <button id= "proj${proj.id}" class="btProjMob" ><span class="TextButtonProjects">See Project</span></button>
+                    <button id= "proj${proj.id}" name="project" class="btProjMob" ><span class="TextButtonProjects">See Project</span></button>
                   </article>`;
   });
 
   portfolio.innerHTML = string;
 
   portfolio.addEventListener('click', (e) => {
-    createStrProjectMob(projects, e.target.id);
+    if (e.target.name === 'project') {
+      createStrProjectMob(projects, e.target.id);
+    }
   });
 };
 
